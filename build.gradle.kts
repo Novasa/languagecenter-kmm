1ba4ce2988cd65e5f1b8e33b54bf895119454c92
Mikkel Schläger <mikkel@novasa.com>
2023-02-09 08:59:56 +0100

    jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,37 @@ plugins {
     id("com.squareup.sqldelight") version Dependencies.SqlDelight.Versions.core apply false
     id("co.touchlab.kermit") version Dependencies.Kermit.version apply false
 
+    kotlin("jvm") version "1.6.0"
+    java
+    `maven-publish`
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.novasa.languagecenter"
+            artifactId = "kmm"
+            version = "0.1"
+
+            from(components["java"])
+        }
+    }
 }
